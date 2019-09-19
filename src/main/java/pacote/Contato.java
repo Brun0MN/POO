24e04fedaf0f317cee64a7b9d188958178c36ec9
Bruno Nascimento <brunomn9@hotmail.com>
2019-09-19 09:21:16 -0300

    criando projeto

---
+++ src/main/java/pacote/Contato.java
@@ -0,0 +1,27 @@
+package pacote;
+
+public class Contato {
+
+    //atributos
+
+    private String nome;
+    private String sobrenome;
+    private String Telefone;
+    private String Email;
+
+
+    //metodos
+
+    public boolean Pessoa(String n, String s){
+        this.nome = n;
+        this.sobrenome = s;
+    }
+
+    public boolean addTelefone(String r, String n){
+
+
+    }
+
+
+
+}
