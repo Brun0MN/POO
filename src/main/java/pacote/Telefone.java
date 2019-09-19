383cf6ffee26c60b667846e0fd3628be1749f924
Bruno Nascimento <brunomn9@hotmail.com>
2019-09-19 09:15:15 -0300

    criando projeto

---
+++ src/main/java/pacote/Telefone.java
@@ -0,0 +1,26 @@
+package pacote;
+
+import java.util.HashMap;
+
+public class Telefone {
+
+    // atributos
+
+    private HashMap<String, String> dados;
+
+
+    // metodos
+
+    public boolean Telefone(){
+
+    }
+
+    public boolean add(String r, String n){
+
+    }
+
+
+
+
+
+}
