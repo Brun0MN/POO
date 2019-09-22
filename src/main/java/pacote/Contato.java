4c2f23d87aba48f674045daaf149af4e92e61a6d
Bruno Nascimento <brunomn9@hotmail.com>
2019-09-21 21:07:50 -0300

    "Telefone, Email e Contato concluídos"

--- src/main/java/pacote/Contato.java
+++ src/main/java/pacote/Contato.java
@@ -1,6 +1,11 @@
 package pacote;
 
+import javax.swing.text.DateFormatter;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Contato {
 
@@ -10,19 +15,81 @@ public class Contato {
     private String sobrenome;
     private LocalDate dataNasc;
     private Telefone telefone;
-    private Email email
+    private Email email;
 
 
     //metodos
 
-    public void Pessoa(String n, String s){
-        this.nome = n;
-        this.sobrenome = s;
+
+
+    public String formata(String mascara, String valor){
+        MaskFormatter mask = null;
+        String resultado = "";
+        try {
+            mask = new MaskFormatter(mascara);
+         //   mask.setValueContainsLiteralCharacters(false);
+            resultado = mask.valueToString(valor);
+        }
+
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+
+
+    public void Contato(String n, String s, String d){
+
+        String nom = formata("??????????????", n);
+        String sob = formata("??????????????", s);
+        String dat = formata("####/##/##", d);
+
+
+        int ano = Integer.parseInt(dat.substring(0,4));
+        int mes = Integer.parseInt(dat.substring(5,7));
+        int dia = Integer.parseInt(dat.substring(8,10));
+
+        //System.out.println(ano);
+        //System.out.println(mes);
+        //System.out.println(dia);
+
+        this.nome = nom;
+        this.sobrenome = sob;
+        this.dataNasc = LocalDate.of(ano, mes, dia);
+
     }
 
     public void addTelefone(String r, String n){
+        Telefone t = new Telefone();
+        t.add(r, n);
 
+    }
 
+    public void addEmail(String r, String e){
+        Email em = new Email();
+        em.add(r, e);
+
+    }
+
+    public void removeTelefone(String r){
+        Telefone t = new Telefone();
+        t.remove(r);
+    }
+
+    public void removeEmail(String r){
+        Email em = new Email();
+        em.remove(r);
+    }
+
+    public void updateTelefone(String r, String n){
+        Telefone t = new Telefone();
+        t.update(r, n);
+    }
+
+    public void updateEmail(String r, String e){
+        Email em = new Email();
+        em.update(r, e);
     }
 
 
--- src/main/java/pacote/Principal.java
+++ src/main/java/pacote/Principal.java
@@ -9,7 +9,13 @@ public class Principal {
         t.update("a", "(48) 99175-6848");
 
         Email e = new Email();
-        e.update("pessoal", "brunomn9@hotmail.com");
+       // e.update("pessoal", "brunomn9@hotmail.com");
+
+        Contato c = new Contato();
+        c.Contato("Bruno", "Martins", "1997/12/23");
+
+
+
 
 
     }
