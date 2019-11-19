a4615324f9794331cd7c08f80f75db380d6d4702
Bruno Nascimento <brunomn9@hotmail.com>
2019-11-19 09:19:39 -0300

    aula 2

--- androidApp2/app/src/main/java/com/example/meuapp2/MainActivity.java
+++ androidApp2/app/src/main/java/com/example/meuapp2/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.meuapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.d(TAG, "Aplicação iniciada");
+
+
     }
 }
